0f6a77ec22be1d4f1b2c24df065163313cdd89bb
Borota Igor <borota.igor@gmail.com>
2021-03-14 14:49:57 +0100

    End of section 12

--- client/src/polyfills.ts
+++ client/src/polyfills.ts
@@ -17,9 +17,10 @@
 /***************************************************************************************************
  * BROWSER POLYFILLS
  */
+ import 'core-js';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
-// import 'classlist.js';  // Run `npm install --save classlist.js`.
+import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /**
  * Web Animations `@angular/platform-browser/animations`
